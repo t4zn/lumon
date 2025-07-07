b342cec9c0421c751f89efee383b2ef47bdf0149
Taizun <165426422+t4zn@users.noreply.github.com>
2025-07-07 15:06:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1401,6 +1401,9 @@ def page1():
 @app.route('/page2')
 def page2():
     return render_template('page2.html')
+@app.route('/health')
+def health():
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
