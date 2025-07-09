7f2c832e9fe619176bfaf122ce0897f2717aa447
Taizun <165426422+t4zn@users.noreply.github.com>
2025-07-09 19:12:19 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -9,7 +9,7 @@ backlog = 2048
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'sync'
 worker_connections = 1000
-timeout = 30
+timeout = 120  # Increased from 30 to 120 seconds
 keepalive = 2
 
 # Logging
